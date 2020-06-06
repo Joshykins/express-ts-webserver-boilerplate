fbf581e70e87b47c544c7dc3474ce8e7289eb18b
Josh Roelle <jroelle@blaze.me>
2020-06-06 00:23:05 -0700

    removed todo list

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-/*
-
-TODO LIST:
-->Proper hotloader
-->Docker Setup
-X ->ESLint
-X ->Start Webserver
-X ->Controllers/Routes/Endpoints
- ->need to integrate DTOS
-->Services
-->Models
- ->DTO  (request/response objects)
- ->DBTO (database objects)
- (DB CONNECTION)
-->ORM (probably typeORM)
-->DI/Testing/Hydration (no idea yet)
-->Logging (logging levels, ERROR, WARN, INFO, DEBUG ) [an enum?]
-->OpenAPI (maybe, haven't looked into feasibility)
-
-*/
 import express from 'express';
 import { PORT } from './config/server-constants';
 import { ExampleRouter } from './endpoints/ExampleEndpoint';
